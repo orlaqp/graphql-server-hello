618cb9426a92eeeaf17e702df25a18e84f27bf10
Orlando Quero <orlaqp@binaryways.com>
2016-10-15 09:56:13 -0400

    sql liste and mongo working

---
+++ apollo-starter-kit/data/schema.js
@@ -0,0 +1,28 @@
+const typeDefinitions = `
+
+type Author {
+  id: Int
+  firstName: String
+  lastName: String
+  posts: [Post]
+}
+
+type Post {
+  id: Int
+  title: String
+  text: String
+  views: Int
+  author: Author
+}
+
+type Query {
+  author(firstName: String, lastName: String): Author
+  getFortuneCookie: String # we'll use this later
+}
+
+schema {
+  query: Query
+}
+`;
+
+export default [typeDefinitions];
